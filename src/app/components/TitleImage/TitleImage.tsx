a207140860a7dabc4d427618200c20c3fa2d1b35
riittagerhards <rgerhards@outlook.com>
2021-11-09 20:52:35 +0100

    🐛 Fix code for svg image path

--- src/app/components/TitleImage/TitleImage.tsx
+++ src/app/components/TitleImage/TitleImage.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import styles from './TitleImage.module.css';
+import TitlePicture from './TitlePicture.svg';
 
 function TitleImage() {
   return (
     <div className={styles.container}>
       <h1>Doc Scanner</h1>
       <div className={styles.imageContainer}>
-        <svg
-          xmlns="TitlePicture.svg"
-          viewBox="0 0 24 24"
-          fill="var(--color-text-primary)"
-        />
+        <img className={styles.picture} src={TitlePicture} />
       </div>
     </div>
   );
