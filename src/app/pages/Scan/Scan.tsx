ad806067d8e245eba8fd65fce1605f58ba52d6b1
riittagerhards <rgerhards@outlook.com>
2021-11-10 11:52:11 +0100

    ✨ Add text scan function on onClick

--- src/app/pages/Scan/Scan.tsx
+++ src/app/pages/Scan/Scan.tsx
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import ImageInput from '../../components/ImageInput/ImageInput';
 import TitleImage from '../../components/TitleImage/TitleImage';
 import styles from './Scan.module.css';
+import Tesseract from 'tesseract.js';
 
 function Scan() {
   const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const [recognizedText, setRecognizedText] = useState<string | null>(null);
 
   return (
     <div className={styles.container}>
@@ -13,9 +15,26 @@ function Scan() {
       ) : (
         <TitleImage />
       )}
-      <ImageInput onUpload={setImageUrl} />
-      <button className={styles.scanButton} disabled={imageUrl === null}>
-        Scan
+      {recognizedText ? (
+        <p>{recognizedText}</p>
+      ) : (
+        <ImageInput onUpload={setImageUrl} />
+      )}
+      <button
+        className={styles.scanButton}
+        disabled={imageUrl === null}
+        onClick={() => {
+          if (imageUrl) {
+            Tesseract.recognize(imageUrl, 'eng', {
+              logger: (message) => console.log(message.progress),
+            }).then((result) => {
+              const text = result.data.text;
+              setRecognizedText(text);
+            });
+          }
+        }}
+      >
+        Scan image
       </button>
       <a className={styles.link} href="#">
         Skip and go to documents
