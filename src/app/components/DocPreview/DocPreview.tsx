b399120b1c7dfc9032d9424d67646810f5b03336
riittagerhards <rgerhards@outlook.com>
2021-11-12 16:00:56 +0100

    ♻️ Refactor mapping

--- src/app/components/DocPreview/DocPreview.tsx
+++ src/app/components/DocPreview/DocPreview.tsx
@@ -1,46 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styles from './DocPreview.module.css';
-import getDocuments from './GetDocuments';
 
-type Document = {
-  id: number;
+export type Document = {
+  id?: number;
   title: string;
   text: string;
 };
 
-function DocPreview(): JSX.Element {
-  const [documents, setDocuments] = useState<null | Document[]>(null);
-
-  useEffect(() => {
-    async function load() {
-      const newDocuments = await getDocuments();
-      setDocuments(newDocuments);
-    }
-    load();
-  }, []);
-
+function DocPreview({ title, text }: Document): JSX.Element {
   return (
-    <section>
-      {documents &&
-        documents.slice(0, 10).map((document) => (
-          <article className={styles.container} key={document.id}>
-            <h3>{document.title}</h3>
-            <p className={styles.text}></p>
-          </article>
-        ))}
-    </section>
+    <div className={styles.card}>
+      <article className={styles.container}>
+        <h3>{title}</h3>
+        <p className={styles.text}>{text}</p>
+      </article>
+      <article className={styles.container}>
+        <h3>{title}</h3>
+        <p className={styles.text}>{text}</p>
+      </article>
+    </div>
   );
 }
 
 export default DocPreview;
-
-/*   <div className={styles.card}>
-      <article className={styles.container}>
-        <h3>{title}</h3>
-        <p className={styles.text}>{text}</p>
-      </article>
-      <article className={styles.container}>
-        <h3>{title}</h3>
-        <p className={styles.text}>{text}</p>
-      </article>
-    </div>*/
--- src/app/pages/Documents/Documents.tsx
+++ src/app/pages/Documents/Documents.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Documents.module.css';
-import DocPreview from '../../components/DocPreview/DocPreview';
 import SearchBar from '../../components/SearchBar/SearchBar';
+import getDocuments from '../../components/DocPreview/GetDocuments';
+import DocPreview, { Document } from '../../components/DocPreview/DocPreview';
 
 function Documents(): JSX.Element {
+  const [documents, setDocuments] = useState<null | Document[]>(null);
+
+  useEffect(() => {
+    async function load() {
+      const newDocuments = await getDocuments();
+      setDocuments(newDocuments);
+    }
+    load();
+  }, []);
+  console.log(documents);
+
   return (
     <div className={styles.container}>
       <SearchBar />
-      <DocPreview />
+      {documents &&
+        documents.map((document) => (
+          <DocPreview
+            title={document.title}
+            text={document.text}
+            key={document.id}
+          />
+        ))}
     </div>
   );
 }
